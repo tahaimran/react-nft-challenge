a632f93783b24396e52d2c061731783c797fb30b
tahaimran <tahaimran.24@gmail.com>
2021-12-16 10:13:54 +0500

    Header Completed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './Header.css'
+import punkLogo from '../assets/header/cryptopunk-logo.png'
+import searchIcon from '../assets/header/search.png'
+import themeSwitcher from '../assets/header/theme-switch.png'
 
 function Header() {
     return (
-        <div>
-            
+        <div className='header'>
+            <div className='logoContainer'>
+            <img src={punkLogo} className='punkLogo'/>
+            </div>
+            <div className='searchBar'>
+                <div className='searchIcon'>
+                    <img src={searchIcon}/>
+                </div>
+                <input className='searchInput' placeholder='Collection, Items or User'/>
+            </div>
+            <div className='headerItems'>
+                 <p>Drops</p>
+                 <p>Marketplace</p>
+                 <p>Create</p>
+            </div>
+            <div className='headerActions'>
+                <div className='themeSwitcher'>
+                    <img src={themeSwitcher}/>
+                </div>
+            </div>
+            <div className='loginButton'>
+                Get In
+            </div>
         </div>
     )
 }
