6d97b4f48a6b5cce8b7459a5cf922040aa6acc4f
tahaimran <tahaimran.24@gmail.com>
2021-12-16 13:21:11 +0500

    card component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css'
 import Header from "./Components/Header";
+import CollectionCard from './Components/CollectionCard';
+import {useState, useEffect} from './react'
 function App() {
   return (
+
     <div className="app">
     <Header />
+    <CollectionCard 
+    id={0}
+     name={'Eye Op Punk'}
+     traits={[{value:5}]} 
+     image='https://ipfs.thirdweb.com/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4' />
     </div>
   );  
 }
